0ed808513dabaa6af2d8271f455b3bdfacf6c7dc
Tejas Sharma <tsharma1704@gmail.com>
2023-04-18 22:30:44 +0530

    Task Completed

--- app/src/main/java/hello/example/app/MainActivity5.kt
+++ app/src/main/java/hello/example/app/MainActivity5.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import hello.example.app.MainActivity
 
@@ -11,15 +12,17 @@ class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main5)
-
+        val roll=findViewById<EditText>(R.id.editTextTextEmailAddress)
         val button = findViewById<Button>(R.id.button2)
         button.setOnClickListener(){
-
-
-            val intent= Intent(this@MainActivity5,MainActivity::class.java)
-            startActivity(intent)
-            finish()
-
+            if(roll.length()<10){
+                Toast.makeText(this,"Enter valid roll no.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this@MainActivity5, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
